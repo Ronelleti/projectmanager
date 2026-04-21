44015126bce72f12bb003dfb1e170559762ac1f1
ron <ronel1354@gmail.com>
2026-04-20 22:19:38 -0300

    add controller

---
+++ src/main/java/ron/com/projectmanager/TestController.java
@@ -0,0 +1,18 @@
+package ron.com.projectmanager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "App is working!";
+    }
+
+    @GetMapping("/tasks")
+    public String tasks() {
+        return "Tasks endpoint working!";
+    }
+}
